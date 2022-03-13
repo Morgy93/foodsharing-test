629bc7ff21ac2f029178f622d52155e73c802f7c
Stefan C <gitlab@stcz.de>
2022-03-13 11:44:04 +0000

    Resolve "tmp Cleanup"

--- sh/fs.maintenance.daily.sh
+++ sh/fs.maintenance.daily.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# cleanup tmp objects
+/usr/bin/find /var/www/production/shared/tmp/ -mtime +2 -type f -delete
+/usr/bin/find /var/www/production/shared/data/mailattach/tmp/ -mtime +2 -type f -delete
+# daily maintenance
 cd /var/www/production/current/
 FS_ENV=prod php run.php Maintenance daily
