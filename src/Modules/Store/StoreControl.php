42bbc4f64f7aa4dcbc35ebc09588bb4b3b67ad2b
Alex <alexander.simm@posteo.de>
2022-11-26 11:56:11 +0000

    link leads back to the store you are currently editing

--- src/Modules/Store/StoreControl.php
+++ src/Modules/Store/StoreControl.php
@@ -121,7 +121,7 @@ class StoreControl extends Control
 			}
 
 			$this->pageHelper->addContent($this->v_utils->v_field($this->v_utils->v_menu([
-				$this->routeHelper->pageLink('betrieb')
+				$this->routeHelper->pageLink('fsbetrieb&id=' . $id)
 			]), $this->translator->trans('storeedit.actions')), CNT_RIGHT);
 		} elseif (isset($_GET['id'])) {
 			$this->routeHelper->go('/?page=fsbetrieb&id=' . (int)$_GET['id']);
